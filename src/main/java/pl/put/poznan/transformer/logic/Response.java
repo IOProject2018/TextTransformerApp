3b0cde02806b3b9fc2606db4b0caf5afe3b13aa9
Michał Bień <michal1.bien@gmail.com>
2018-10-30 18:24:52 +0100

    Zmiany na podstawie komentarzy do PR26

--- src/main/java/pl/put/poznan/transformer/logic/Response.java
+++ src/main/java/pl/put/poznan/transformer/logic/Response.java
@@ -1,16 +1,8 @@
 package pl.put.poznan.transformer.logic;
 
 public class Response {
-    private String original;
     private String transformed;
 
-    public String getOriginal() {
-        return original;
-    }
-
-    public void setOriginal(String original) {
-        this.original = original;
-    }
 
     public void setTransformed(String transformed) {
         this.transformed = transformed;
@@ -20,8 +12,7 @@ public class Response {
         return transformed;
     }
 
-    public Response(String original, String transformed) {
-        this.original = original;
+    public Response(String transformed) {
         this.transformed = transformed;
     }
 }
