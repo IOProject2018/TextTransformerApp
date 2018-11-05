852f0fb6b0d5523b0b527fc7f6328d9050feb4ae
Krzysztof Łaganowski <krzysztof.laganowski@gmail.com>
2018-11-05 18:49:45 +0100

    poprawiona literówka "LatexSymbolsTransformer"

--- src/main/java/pl/put/poznan/transformer/logic/LatexSymbolsTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/LatexSymbolsTransformer.java
@@ -2,7 +2,7 @@ package pl.put.poznan.transformer.logic;
 /**
  *  Class used to make 'latex like' transformation
  */
-public class LatexSymbolsTrarnsformer implements TextTransformerInterface {
+public class LatexSymbolsTransformer implements TextTransformerInterface {
     public String transform(String text) {
             text = text.replace("&", "\\&");
             text = text.replace("$", "\\$");
