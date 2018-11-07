cbe0262cb20ec6e9a2b2d7c134a600a7e3d0939e
maartynaa <44441407+maartynaa@users.noreply.github.com>
2018-11-07 17:17:54 +0100

    Spacje
    Zgodnie ze wskazówkami Mikołaja :)

--- src/main/java/pl/put/poznan/transformer/logic/ToShortcutTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/ToShortcutTransformer.java
@@ -15,25 +15,24 @@ public class ToShortcutTransformer implements TextTransformerInterface {
      * @return text without shortcuts
      */
     public String transform(String text) {
-        text = text.replaceAll("np.", "na przykład");
-        text = text.replaceAll("Np.", "Na przykład");
-        text = text.replaceAll("NP.", "NA PRZYKŁAD");
-        text = text.replaceAll("m.in.", "między innymi");
-        text = text.replaceAll("M.in.", "Między innymi");
-        text = text.replaceAll("M.IN.", "MIĘDZY INNYMI");
-        text = text.replaceAll("itp.", "i tym podobne");
-        text = text.replaceAll("Itp.", "I tym podobne");
-        text = text.replaceAll("ITP.", "I TYM PODOBNE");
-        text = text.replaceAll("itd.", "i tak dalej");
-        text = text.replaceAll("Itd.", "I tak dalej");
-        text = text.replaceAll("ITD.", "I TAK DALEJ");
-        text = text.replaceAll("prof.", "profesor");
-        text = text.replaceAll("Prof.", "Profesor");
-        text = text.replaceAll("PROF.", "PROFESOR");
-        text = text.replaceAll("dr", "doktor");
-        text = text.replaceAll("Dr", "Doktor");
-        text = text.replaceAll("DR", "DOKTOR");
-
+         text = text.replaceAll(" np. ", " na przykład ");
+        text = text.replaceAll(" Np. ", " Na przykład ");
+        text = text.replaceAll(" NP. ", " NA PRZYKŁAD ");
+        text = text.replaceAll(" m.in. ", " między innymi ");
+        text = text.replaceAll(" M.in. ", " Między innymi ");
+        text = text.replaceAll(" M.IN. ", " MIĘDZY INNYMI ");
+        text = text.replaceAll(" itp. ", " i tym podobne ");
+        text = text.replaceAll(" Itp. ", " I tym podobne ");
+        text = text.replaceAll(" ITP. ", " I TYM PODOBNE ");
+        text = text.replaceAll(" itd. ", " i tak dalej ");
+        text = text.replaceAll(" Itd. ", " I tak dalej ");
+        text = text.replaceAll(" ITD. ", " I TAK DALEJ ");
+        text = text.replaceAll(" prof. ", " profesor ");
+        text = text.replaceAll(" Prof. ", " Profesor ");
+        text = text.replaceAll(" PROF. ", " PROFESOR ");
+        text = text.replaceAll(" dr ", " doktor ");
+        text = text.replaceAll(" Dr ", " Doktor ");
+        text = text.replaceAll(" DR ", " DOKTOR ");
         return text;
     }
 
