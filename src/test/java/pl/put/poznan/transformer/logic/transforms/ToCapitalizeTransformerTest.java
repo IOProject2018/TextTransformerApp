b91f40214722a5db32b625f07d282f491a3e367a
Mikołaj Wolicki <wolicki.mikolaj@gmail.com>
2018-11-28 15:24:55 +0100

    Dodaj 3 testy - wedlug schematu Michala

---
+++ src/test/java/pl/put/poznan/transformer/logic/transforms/ToCapitalizeTransformerTest.java
@@ -0,0 +1,25 @@
+package pl.put.poznan.transformer.logic.transforms;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.put.poznan.transformer.logic.TextTransformerImpl;
+import pl.put.poznan.transformer.logic.TextTransformerInterface;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ToCapitalizeTransformerTest {
+    private TextTransformerInterface transformer;
+
+    @Before
+    public void setupTransformer() {
+        transformer = new ToCapitalizeTransformer(new TextTransformerImpl());
+    }
+
+    @Test
+    public void shouldReturnLowerText() {
+        String text = "pierwsze litery male";
+        String result = "Pierwsze Litery Male";
+
+        assertThat(transformer.transform(text)).isEqualTo(result);
+    }
+}
