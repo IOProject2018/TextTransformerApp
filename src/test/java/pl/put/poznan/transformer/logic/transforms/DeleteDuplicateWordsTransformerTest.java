c805a0092b7a625ba6e5c0dddf8b6e76a808c875
maartynaa <martyna.kr.maciejewska@student.put.poznan.pl>
2019-01-01 21:57:13 +0100

    Tests with Mockito

--- src/test/java/pl/put/poznan/transformer/logic/transforms/DeleteDuplicateWordsTransformerTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/transforms/DeleteDuplicateWordsTransformerTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 import pl.put.poznan.transformer.logic.TextTransformerImpl;
 import pl.put.poznan.transformer.logic.TextTransformerInterface;
 
@@ -28,5 +29,33 @@ public class DeleteDuplicateWordsTransformerTest
         String result = "raz dwa";
 
         assertThat(transformer.transform(text)).isEqualTo(result);
+    }
+
+    /**
+     * mockito tests
+     */
+
+    @Test
+    public void testTransformNormalText() {
+        TextTransformerInterface textTransformerInterface = mock(TextTransformerInterface.class);
+        when(textTransformerInterface.transform("bla bla")).thenReturn("bla");
+
+        DeleteDuplicateWordsTransformer text = new DeleteDuplicateWordsTransformer(textTransformerInterface);
+        String result = text.transform("bla bla");
+
+        verify(textTransformerInterface).transform("bla bla");
+        assertEquals("bla", result);
+    }
+
+    @Test
+    public void testTransformUpperText() {
+        TextTransformerInterface textTransformerInterface = mock(TextTransformerInterface.class);
+        when(textTransformerInterface.transform("COS COS TAM")).thenReturn("COS TAM");
+
+        DeleteDuplicateWordsTransformer text = new DeleteDuplicateWordsTransformer(textTransformerInterface);
+        String result = text.transform("COS COS TAM");
+
+        verify(textTransformerInterface).transform("COS COS TAM");
+        assertEquals("COS TAM", result);
     }
 }
--- src/test/java/pl/put/poznan/transformer/logic/transforms/DeletePLSymbolsTransformerTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/transforms/DeletePLSymbolsTransformerTest.java
@@ -6,6 +6,8 @@ import pl.put.poznan.transformer.logic.TextTransformerImpl;
 import pl.put.poznan.transformer.logic.TextTransformerInterface;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class DeletePLSymbolsTransformerTest {
     private TextTransformerInterface transformer;
@@ -21,5 +23,45 @@ public class DeletePLSymbolsTransformerTest {
         String result = "tekst z polskimi znakami a e";
 
         assertThat(transformer.transform(text)).isEqualTo(result);
+    }
+
+    /**
+     * mockito tests
+     */
+
+    @Test
+    public void testTransformNormalText() {
+        TextTransformerInterface textTransformerInterface = mock(TextTransformerInterface.class);
+        when(textTransformerInterface.transform("są się łoś iść żółw")).thenReturn("sa sie los isc zolw");
+
+        DeletePLSymbolsTrarnsformer text = new DeletePLSymbolsTrarnsformer(textTransformerInterface);
+        String result = text.transform("są się łoś iść żółw");
+
+        verify(textTransformerInterface).transform("są się łoś iść żółw");
+        assertEquals("sa sie los isc zolw", result);
+    }
+
+    @Test
+    public void testTransformUpperText() {
+        TextTransformerInterface textTransformerInterface = mock(TextTransformerInterface.class);
+        when(textTransformerInterface.transform("ŚNI GĘŚ")).thenReturn("SNI GES");
+
+        DeletePLSymbolsTrarnsformer text = new DeletePLSymbolsTrarnsformer(textTransformerInterface);
+        String result = text.transform("ŚNI GĘŚ");
+
+        verify(textTransformerInterface).transform("ŚNI GĘŚ");
+        assertEquals("SNI GES", result);
+    }
+
+    @Test
+    public void testTransformCapitalizeText() {
+        TextTransformerInterface textTransformerInterface = mock(TextTransformerInterface.class);
+        when(textTransformerInterface.transform("Koń Ćmy Ósmy")).thenReturn("Kon Cmy Osmy");
+
+        DeletePLSymbolsTrarnsformer text = new DeletePLSymbolsTrarnsformer(textTransformerInterface);
+        String result = text.transform("Koń Ćmy Ósmy");
+
+        verify(textTransformerInterface).transform("Koń Ćmy Ósmy");
+        assertEquals("Kon Cmy Osmy", result);
     }
 }
