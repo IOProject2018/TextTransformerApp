358bf54b2ef954b20c729370b899c205f50e0223
maartynaa <martyna.kr.maciejewska@student.put.poznan.pl>
2019-01-05 00:29:59 +0100

    Tests with Mockito

--- src/test/java/pl/put/poznan/transformer/logic/transforms/NumToStringTransformerTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/transforms/NumToStringTransformerTest.java
@@ -6,6 +6,8 @@ import pl.put.poznan.transformer.logic.TextTransformerImpl;
 import pl.put.poznan.transformer.logic.TextTransformerInterface;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class NumToStringTransformerTest {
     private TextTransformerInterface transformer;
@@ -59,5 +61,57 @@ public class NumToStringTransformerTest {
         String result = "sto dwadzieścia siedem tysięcznych";
 
         assertThat(transformer.transform(text)).isEqualTo(result);
+    }
+
+    /**
+     * mockito tests
+     */
+
+    @Test
+    public void testTransformParseComposedNumber() {
+        TextTransformerInterface textTransformerInterface = mock(TextTransformerInterface.class);
+        when(textTransformerInterface.transform("781.135")).thenReturn("siedemset osiemdziesiąt jeden i sto trzydzieści pięć tysięcznych");
+
+        NumToStringTransformer text = new NumToStringTransformer(textTransformerInterface);
+        String result = text.transform("781.135");
+
+        verify(textTransformerInterface).transform("781.135");
+        assertEquals("siedemset osiemdziesiąt jeden i sto trzydzieści pięć tysięcznych", result);
+    }
+
+    @Test
+    public void testTransformRestLimitExceeded() {
+        TextTransformerInterface textTransformerInterface = mock(TextTransformerInterface.class);
+        when(textTransformerInterface.transform("0.135111")).thenReturn("sto trzydzieści pięć tysięcznych");
+
+        NumToStringTransformer text = new NumToStringTransformer(textTransformerInterface);
+        String result = text.transform("0.135111");
+
+        verify(textTransformerInterface).transform("0.135111");
+        assertEquals("sto trzydzieści pięć tysięcznych", result);
+    }
+
+    @Test
+    public void testTransformLimitExceeded() {
+        TextTransformerInterface textTransformerInterface = mock(TextTransformerInterface.class);
+        when(textTransformerInterface.transform("mam 12345678 zl.")).thenReturn("mam 12345678 zl.");
+
+        NumToStringTransformer text = new NumToStringTransformer(textTransformerInterface);
+        String result = text.transform("mam 12345678 zl.");
+
+        verify(textTransformerInterface).transform("mam 12345678 zl.");
+        assertEquals("mam 12345678 zl.", result);
+    }
+
+    @Test
+    public void testTransformNumToString() {
+        TextTransformerInterface textTransformerInterface = mock(TextTransformerInterface.class);
+        when(textTransformerInterface.transform("123.0")).thenReturn("sto dwadzieścia trzy i zero");
+
+        NumToStringTransformer text = new NumToStringTransformer(textTransformerInterface);
+        String result = text.transform("123.0");
+
+        verify(textTransformerInterface).transform("123.0");
+        assertEquals("sto dwadzieścia trzy i zero", result);
     }
 }
